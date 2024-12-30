9342e66a73d081c7896596f717314378f8c5838b
KIBA <kjh030529@gmail.com>
2024-12-30 19:13:57 +0900

    [프로젝트] 어셈블리 업데이트

--- obj/Debug/net8.0/KIBAEMON2024-Music.AssemblyInfo.cs
+++ obj/Debug/net8.0/KIBAEMON2024-Music.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KIBAEMON2024-Music")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab93a7f85128c898f4178296c7c79a3cab041887")]
 [assembly: System.Reflection.AssemblyProductAttribute("KIBAEMON2024-Music")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KIBAEMON2024-Music")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
